6d3af92080da52699af1a50bfd91166b489a1644
Ghayeth Al-Masri <amalj6570@gmail.com>
2022-04-03 18:07:26 +0300

    main template

---
+++ resources/views/homeTrails/page-banner.blade.php
@@ -0,0 +1,17 @@
+<!-- Start: Page Banner -->
+<section class="page-banner services-banner">
+    <div class="container">
+        <div class="banner-header">
+            <h2>Books & Media Listing</h2>
+            <span class="underline center"></span>
+            <p class="lead">Proin ac eros pellentesque dolor pharetra tempo.</p>
+        </div>
+        <div class="breadcrumb">
+            <ul>
+                <li><a href="index-2.html">Home</a></li>
+                <li>Books & Media</li>
+            </ul>
+        </div>
+    </div>
+</section>
+<!-- End: Page Banner -->
